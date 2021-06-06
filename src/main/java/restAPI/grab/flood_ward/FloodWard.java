1b63bac4cb25c6065e4745a6315d471793712736
xuan-vy-nguyen <xuanvy99@gmail.com>
2021-06-06 14:41:48 +0700

    fix bug get list registration

--- src/main/java/restAPI/grab/flood_ward/FloodWard.java
+++ src/main/java/restAPI/grab/flood_ward/FloodWard.java
@@ -65,15 +65,18 @@ public class FloodWard {
 
                 if (regisPeek.getDistance2Rescuer() > curDistance) {
 
+                    System.out.println("    -> size == maxSize: remove top("+regisPeek.getDistance2Rescuer()
+                            +") and add("+item.getDistance2Rescuer()+")");
+
                     regisPeek.setRescuerUsername(null);
                     regisPeek.setDistance2Rescuer(0);
+
+                    regisForRescuer.poll();
 
                     item.setRescuerUsername(floodRescuer.rescuerId);
                     item.setDistance2Rescuer(curDistance);
 
                     regisForRescuer.add(item);
-                    System.out.println("    -> size == maxSize: remove top("+regisPeek.getDistance2Rescuer()
-                            +") and add("+item.getDistance2Rescuer()+")");
 
                 } else {
                     System.out.println("    -> cannot add because distance is not small enough");
