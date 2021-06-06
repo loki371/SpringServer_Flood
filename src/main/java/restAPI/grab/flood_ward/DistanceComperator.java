6778817c3b6b27827683524e64e5d4ca205b93b1
xuan-vy-nguyen <xuanvy99@gmail.com>
2021-06-06 14:37:59 +0700

    fix distqance again

--- src/main/java/restAPI/grab/flood_ward/DistanceComperator.java
+++ src/main/java/restAPI/grab/flood_ward/DistanceComperator.java
@@ -7,9 +7,9 @@ import java.util.Comparator;
 public class DistanceComperator implements Comparator<FloodRegistration> {
     @Override
     public int compare(FloodRegistration o1, FloodRegistration o2) {
-        if (o1.getDistance2Rescuer() < o2.getDistance2Rescuer())
-            return -1;
         if (o1.getDistance2Rescuer() > o2.getDistance2Rescuer())
+            return -1;
+        if (o1.getDistance2Rescuer() < o2.getDistance2Rescuer())
             return 1;
         return 0;
     }
